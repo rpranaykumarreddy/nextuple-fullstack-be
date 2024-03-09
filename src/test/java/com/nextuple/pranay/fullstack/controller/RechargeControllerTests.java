03bd9e08fe45f6bed647b4e732632263972577dd
Ravula Pranay Kumar Reddy <rpranayreddy40@gmail.com>
2024-03-09 17:52:37 +0530

    recharge amount validation testing

--- src/test/java/com/nextuple/pranay/fullstack/controller/RechargeControllerTests.java
+++ src/test/java/com/nextuple/pranay/fullstack/controller/RechargeControllerTests.java
@@ -2,6 +2,7 @@ package com.nextuple.pranay.fullstack.controller;
 
 
 import com.nextuple.pranay.fullstack.TestUtil;
+import com.nextuple.pranay.fullstack.exception.CustomException;
 import com.nextuple.pranay.fullstack.service.RechargeService;
 import com.nextuple.pranay.fullstack.utils.AuthUserUtils;
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,12 @@ public class RechargeControllerTests {
         when(authUserUtils.getUserId(anyString())).thenReturn(TestUtil.USER1_NAME);
         when(rechargeService.rechargeWallet(anyString(), anyDouble())).thenReturn(new ResponseEntity<>(TestUtil.WalletTestData.getWallet1ResponseDto(), HttpStatus.OK));
         assertDoesNotThrow(() -> rechargeController.rechargeWallet(TestUtil.TOKEN, 30000.3));
+    }
+    @Test
+    public void testRechargeWallet_amountValidation() {
+        when(authUserUtils.getUserId(anyString())).thenReturn(TestUtil.USER1_NAME);
+        CustomException.ValidationException exception = assertThrows(CustomException.ValidationException.class, () -> rechargeController.rechargeWallet(TestUtil.TOKEN, 0.3));
+        assertEquals("Your can only recharge with more than ₹1", exception.getMessage());
     }
 
 }
